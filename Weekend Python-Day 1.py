e7fc7b1208902b227d24607dd0f79215bfb706e3
myfriendprawin <53305268+myfriendprawin@users.noreply.github.com>
2019-07-25 22:21:04 +0800

    Add files via upload

---
+++ Weekend Python-Day 1.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[15]:
+
+
+#introduction to variables:
+X=1;
+Y=3;
+Name="Praveen Kumar Vattikunta"
+print( "X+Y=",X+Y) 
+print('Name:',Name)
+
+
+# In[20]:
+
+
+Name_new="raj kumar"
+Name_New1="RAJ KUMAR"
+print(Name_new.title())
+print(Name_New1.title())
+
+
+# In[21]:
+
+
+#string to upper case
+name="praveen"
+print(name.upper())
+
+
+# In[26]:
+
+
+#F-staings:
+first_name="Prabhu"
+last_name="Dev"
+full_name=f"{first_name} {last_name}"
+print(full_name)
+
+
+# In[29]:
+
+
+#Adding spaces to strings
+print('string')
+print('\nstring')
+print('\tstring')
+
+
+# In[30]:
+
+
+#string with apostophe
+print("It's my python practice Day1")
+
+
+# In[34]:
+
+
+#Using \n, \t
+print("Name: Praveen\n\t\tKumar\n\t\t\tVattikunta")
+
+
+# In[38]:
+
+
+# Remove white space
+Var1="Variable "
+print(Var1)
+
