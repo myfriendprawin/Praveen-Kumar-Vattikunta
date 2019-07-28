86e5b0ce19c0604107599f6200a486b534ffaa46
myfriendprawin <53305268+myfriendprawin@users.noreply.github.com>
2019-07-28 21:42:56 +0800

    Add files via upload

---
+++ Weekend Day2.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+#Data Types: 
+#Numbers: Number data types are immutables.
+#Integers:
+A=1
+B=3
+A+B
+
+
+# In[7]:
+
+
+#Float: Declaring values in decimal is called as float.
+X=1.
+Y=2.0
+X+Y
+
+
+# In[8]:
+
+
+#Advance assignment of values to variables.
+K,L,M,N=5,6,9,1
+print(M)
+
+
+# In[19]:
+
+
+#List Data Type: List is muatble. we can modify/change/append values to it.
+#List is a collection of items in order
+Names=['Raj', 'Vijay','akmal', 'Shuwen']
+#        0        1      2         3           ==> this is called as indexing.
+# in above list, the names are arranged in an order. 
+print(Names)
+print(Names[1])
+print(Names[3])
+print(Names[2].upper())
+
